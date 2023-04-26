4a8e19daa2161da8b994134a8b06e4642e9d25a1
xadityaxyz <aadiwadekar47@gmail.com>
2023-04-26 18:34:10 +0530

    one of the last a

--- helloworld.java
+++ helloworld.java
@@ -1,5 +1,5 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello with chnages new Java");  
+     System.out.println("Hello with a chnages new Java");  
     }  
 }  
